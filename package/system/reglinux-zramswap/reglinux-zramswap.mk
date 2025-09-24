1ed0bce0c6d0d02edb5fb13844298d4e197b84ee
cjom <55603581+cjom@users.noreply.github.com>
2025-09-24 16:47:21 +0100

    Optimize init.d scripts for dash shell

--- package/system/reglinux-zramswap/reglinux-zramswap.mk
+++ package/system/reglinux-zramswap/reglinux-zramswap.mk
@@ -10,7 +10,7 @@ REGLINUX_ZRAMSWAP_SITE =
 
 define REGLINUX_ZRAMSWAP_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/share/reglinux/services
-	$(INSTALL) -Dm755 $(BR2_EXTERNAL_REGLINUX_PATH)/package/system/reglinux-zramswap/zramswap $(TARGET_DIR)/usr/share/reglinux/services/
+	$(INSTALL) -Dm755 $(BR2_EXTERNAL_REGLINUX_PATH)/package/system/reglinux-zramswapenable_ZRAMswap $(TARGET_DIR)/usr/share/reglinux/services/
 endef
 
 $(eval $(generic-package))
